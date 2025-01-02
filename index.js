84782ad35581546b5b64095412387ad27246b900
Deep shah <deepshah2805@gmail.com>
2025-01-02 15:50:39 +0530

    Revert "feat: jaeger implementation"
    This reverts commit ff46da081c4923cf0310252a07bade860cb4ebd9.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("./tracing");
 const express = require("express");
 const schema = require("./schema/schema");
 const { graphqlHTTP } = require("express-graphql");
@@ -6,7 +5,6 @@ const { initializeDB } = require("./config/db");
 const cors = require("cors");
 const metricsRouter = require("./routes/metrics");
 const telemetryMiddleware = require("./middleware/telemetryMiddleware");
-const tracingMiddleware = require("./middleware/tracingMiddleware");
 
 const app = express();
 const PORT = 5000;
@@ -23,7 +21,6 @@ app.use((err, req, res, next) => {
 
 // API routes
 app.use(telemetryMiddleware);
-app.use(tracingMiddleware);
 app.use("/", metricsRouter);
 app.use(
   "/graphql",
